9af1441df6094edccfcd7d8e46e66387b7e5cba8
Juyeon Kang <kangj469@newschool.edu>
2019-03-08 02:36:36 -0500

    sdafasd

---
+++ project2/recipe3/main.js
@@ -0,0 +1,21 @@
+console.log('hello world');
+
+function myFunction() {
+  var x = document.getElementById("topnav");
+  if (x.className === "sidenav") {
+    x.className += "responsive";
+  } else {
+    x.className = "sidenav";
+  }
+}
+
+
+
+
+
+     
+
+
+
+
+
