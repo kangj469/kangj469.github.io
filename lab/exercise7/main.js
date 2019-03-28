6f460dcc2cd0b0255186a4ea947606e6b685eff7
Juyeon Kang <kangj469@newschool.edu>
2019-03-28 11:14:00 -0400

    dfd

--- lab/exercise7/main.js
+++ lab/exercise7/main.js
@@ -1,5 +1,13 @@
 console.log('hello world');
 
+var $sound = $("<audio preload=auto>");
+     $sound.attr("src",
+        "https://www.kasandbox.org/programming-sounds/rpg/door-open.mp3");
+        $("#leafers").on("click", function(event) {
+            // Do something!
+         $("body").append($sound);
+        $sound[0].play();})
+
    $("html").on("click", function(event) {
         var $dot = $("<div></div>");
         $dot.addClass("star");
