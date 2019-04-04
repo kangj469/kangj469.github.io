f857c9862c197b2fdc82fdafda1bacafe0a0058b
Juyeon Kang <kangj469@newschool.edu>
2019-04-04 11:39:12 -0400

    dasfd

---
+++ lab/exercise8/main.js
@@ -0,0 +1,51 @@
+/* 2 */
+alert ("Welcome! Please view console on your broswer")
+
+/* 3 */
+var myName = "Juyeon Kang";
+
+var myAge = 20;
+
+var myBirthday = "My birthday is September 3rd";
+
+var admin = false;
+
+var myself = myName + myAge;
+
+/* 4 */
+var results =   1 + 19, 10 * 2, 15 + 5, 30 - 10, 40 / 2;
+console.log(results);
+
+/* 5 */
+var countries = ["UK", "Italy", "Iceland", "Taiwan"]
+console.log(countries[0]);
+console.log(countries[countries.length]);
+
+/* 6 */
+var currentTime = "The current time is" + time;
+var time = new Date();
+
+/* 7 */
+var currenHour = new Date().getHours();
+
+if (currenHour = 11) {
+  console.log("It is 11 now")
+} else {
+  console.log("It is not 11 yet")
+}
+
+/* 8 */
+console.log (myName.length);
+myName = myName[3-1];
+
+if (myName <= 9) {
+  console.log("My name is relatively short.")
+} (myName > 14) {
+  console.log("My name is relatively long.")
+} else {
+  console.log("My name is neither short nor long.")
+}
+
+
+
+
