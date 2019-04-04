06485ca9c2e9dc2aa694e8f95a51411762a89597
Juyeon Kang <kangj469@newschool.edu>
2019-04-04 11:52:01 -0400

    adsfas

--- lab/exercise8/main.js
+++ lab/exercise8/main.js
@@ -1,5 +1,5 @@
 /* 2 */
-alert ("Welcome! Please view console on your broswer")
+alert("Welcome! Please view console on your broswer");
 
 /* 3 */
 var myName = "Juyeon Kang";
@@ -13,26 +13,30 @@ var admin = false;
 var myself = myName + myAge;
 
 /* 4 */
-var results =   1 + 19, 10 * 2, 15 + 5, 30 - 10, 40 / 2;
-console.log(results);
+console.log(1 + 19);
+console.log(10 * 2);
+console.log(15 + 5);
+console.log(30 - 10);
+console.log(40 / 2);
+
 
 /* 5 */
 var countries = ["UK", "Italy", "Iceland", "Taiwan"]
 console.log(countries[0]);
-console.log(countries[countries.length]);
+console.log(countries.length);
 
 /* 6 */
 var currentTime = "The current time is" + time;
 var time = new Date();
 
 /* 7 */
-var currenHour = new Date().getHours();
+var currentHour = new Date().getHours();
 
-if (currenHour = 11) {
+if (currentHour = 11) {
   console.log("It is 11 now")
 } else {
   console.log("It is not 11 yet")
-}
+};
 
 /* 8 */
 console.log (myName.length);
@@ -40,11 +44,11 @@ myName = myName.length - 1;
 
 if (myName <= 9) {
   console.log("My name is relatively short.")
-} (myName > 14) {
+} else if (myName >= 14) {
   console.log("My name is relatively long.")
 } else {
   console.log("My name is neither short nor long.")
-}
+};
 
 
 
