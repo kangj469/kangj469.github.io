ad337c917ce58a28579178eb0dfdfdbcff173935
Juyeon Kang <kangj469@newschool.edu>
2019-04-18 00:45:30 -0400

    jnnlk

---
+++ lab/exercise9/main.js
@@ -0,0 +1,35 @@
+console.log('hello world');
+
+
+/*current time*/
+
+function clock() {
+
+  var date = new Date(),
+      hour = date.getHours(),
+      minute = checkTime(date.getMinutes()),
+      second = checkTime(date.getSeconds());
+
+  function checkTime(i) {
+    if( i < 10 ) {
+      i = "0" + i;
+    }
+    return i;
+  }
+
+if ( hour > 12 ) {
+  hour = hour - 12;
+  if ( hour == 12 ) {
+    hour = checkTime(hour);
+  document.getElementById("time").innerHTML = hour+":"+minute+":"+second+" AM";
+  }
+  else {
+    hour = checkTime(hour);
+    document.getElementById("time").innerHTML = hour+":"+minute+":"+second+" PM";
+  }
+}
+else {
+  document.getElementById("time").innerHTML = hour+":"+minute+":"+second+" AM";;
+}
+var timeout = setTimeout(clock,1000);
+}
